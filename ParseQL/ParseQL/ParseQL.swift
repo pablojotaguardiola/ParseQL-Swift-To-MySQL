28e9f0148903c963f9b6b95bc2635c9a2c77fa19
Pablo <pablojotaguardiola@gmail.com>
2016-06-29 23:30:38 +0200

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit d724f2298cb5c3673053bb113ccde332f6ae1555, reversing
    changes made to 269f2b3eac7c82e9d6a24a0e737f1cf8706abf0e.

--- ParseQL/ParseQL/ParseQL.swift
+++ ParseQL/ParseQL/ParseQL.swift
@@ -71,7 +71,6 @@ public class PQL {
                     
                     if error == nil {
                         //CORRECT
-                        print((NSString(data: data!, encoding: NSUTF8StringEncoding)! as String))
                     }
                     else {
                         print("error")
@@ -109,7 +108,6 @@ public class PQL {
                 }
                 
                 task = session.uploadTaskWithRequest(request, fromData: data, completionHandler: {data, response, error in
-                    
                     
                     if error == nil {
                         let responseDictionary = self.convertStringToDictionary((NSString(data: data!, encoding: NSUTF8StringEncoding)! as String) as String)
@@ -358,11 +356,7 @@ public class PQL {
     }
     
     //UPDATE OR CREATE
-<<<<<<< HEAD
     public func updateOrCreate() {
-=======
-    public func updateOrCreateWithBlock() {
->>>>>>> origin/master
         let config = NSURLSessionConfiguration.defaultSessionConfiguration()
         config.HTTPAdditionalHeaders = ["Accept": "application/json",
                                         "Content-Type": "application/json"
